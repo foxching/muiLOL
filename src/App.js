f8f35328323d9cf46c067092a8327c2e2afd2fa4
foxching <rechielagria@gmail.com>
2020-11-02 21:22:08 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { createMuiTheme, CssBaseline, ThemeProvider as MuiTheme } from "@material-ui/core";
+import React from "react";
+import LoginLayout from "./components/LoginLayout";
+
+const palette = {
+  primary: {
+    main: "#000000",
+    contrastText: "#ffffff"
+  },
+  secondary: {
+    main: "#CA3135",
+    contrastText: "#ffffff"
+  }
+};
+
+const theme = createMuiTheme({
+  palette: palette
+});
+
+const App = () => {
+  return (
+    <MuiTheme theme={theme}>
+      <CssBaseline />
+      <LoginLayout />
+    </MuiTheme>
+  );
+};
+
+export default App;
